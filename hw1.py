39d278ecc5b673439aad82e58b60c97e7c102b80
Behnam <behnam@cs.tufts.edu>
2017-09-25 18:12:07 -0400

    runs on homework

--- hw1.py
+++ hw1.py
@@ -9,7 +9,7 @@ if RUNNING_ON_SERVER:
     # os.environ['WEKADATA']='/r/aiml/ml-software/weka-3-6-11/data/'
     print "SHELL: {}".format(os.environ['SHELL'])
     print "CLASSPATH: {}".format(os.environ['CLASSPATH'])
-    WEKA_DATA_PATH = os.getenv('WEKADATA')
+    WEKA_DATA_PATH = '/r/tcal/work/behnam/135/hw1/data/'
     print "wekadata: {}".format(WEKA_DATA_PATH)
     # WEKA = os.getenv('WEKAINSTALL', '/home/behnam/tufts/135/weka-3-9-1/weka.jar')
     # print "weka: {}".format(WEKA)
@@ -33,19 +33,21 @@ def check_environment():
     return
 
 def run_command(str_cmd):
+    print "******** RUNNING COMMAND: {} ********".format(str_cmd)
     (status, str_output) = commands.getstatusoutput(str_cmd)
     if status != 0:
         print "error in running bash command: {}".format(status)
         print str_output
         exit(1)
 
+    print str_output
+
     return str_output
 
 
 def run_train_test(classifier, train_data, test_data):
-
-    cmd = "java {}.{} -t {}\/{} -T {}\/{}".format(WEKA, classifier, WEKA_DATA_PATH, train_data,WEKA_DATA_PATH, test_data)
-    return run_command(cmd)
+    cmd = "java {}.{} -t {}{} -T {}{}".format(WEKA, classifier, WEKA_DATA_PATH, train_data,WEKA_DATA_PATH, test_data)
+    run_command(cmd)
 
 
 run_train_test(CLASSIFIER_J48, DATA_TRAIN_14, DATA_TEST_14)
