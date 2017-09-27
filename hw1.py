f7c15dac8ca61207b8b2ec97199a3af3018159e5
campiador <behnam.heydarshahi@gmail.com>
2017-09-27 15:16:31 -0400

    part 2 finished

--- hw1.py
+++ hw1.py
@@ -2,13 +2,16 @@ import commands
 import os
 
 from plot import plot_accuracies
+from plot import plot_accuracies_with_stderr
 
 import random
 
 import numpy
 
+
 RUNNING_ON_SERVER = False
-DEBUGGING = True
+CLIENT_DEBUG = False
+DEVELOPER_DEBUG = True
 
 
 CLASSIFIER_J48 = "classifiers.trees.J48"
@@ -51,12 +54,12 @@ def set_environment():
         # For running on Homework server
         # os.environ['CLASSPATH']='/r/aiml/ml-software/weka-3-6-11/weka.jar' + ":" + os.environ['CLASSPATH']
         # os.environ['WEKADATA']='/r/aiml/ml-software/weka-3-6-11/data/'
-        if DEBUGGING:
+        if CLIENT_DEBUG:
             print "SHELL: {}".format(os.environ['SHELL'])
         CLASSPATH = os.environ['CLASSPATH']
         WEKA_DATA_PATH = '/r/tcal/work/behnam/135/hw1/data/'
         # os.unsetenv('CLASSPATH:', CLASSPATH + ":{}{}".format(WE))
-        if DEBUGGING:
+        if CLIENT_DEBUG:
             print "CLASSPATH: {}".format(CLASSPATH)
             print "wekadata: {}".format(WEKA_DATA_PATH)
         # WEKA = os.getenv('WEKAINSTALL', '/home/behnam/tufts/135/weka-3-9-1/weka.jar')
@@ -75,22 +78,20 @@ def set_environment():
         WEKA = "weka"
 
 
-
-
 def test_environment():
-    if DEBUGGING:
+    if CLIENT_DEBUG:
         print "CLASSPATH: {}".format(CLASSPATH)
     return
 
 
 def run_command(str_cmd):
-    if DEBUGGING:
+    if CLIENT_DEBUG:
         print "**** RUNNING COMMAND:\n{} ****".format(str_cmd)
     (status, str_output) = commands.getstatusoutput(str_cmd)
     if status != 0:
         print "error in running bash command: {}".format(status)
         print str_output
-        exit(1)
+        exit(2)
 
     # print str_output
 
@@ -127,7 +128,7 @@ def run_all_train_test_return_accuracies(classifier):
     for i in range(0, n_sets):
         accuracy = run_train_test_return_accuracy(classifier, all_train_sets[i],all_test_sets[i])
         accuracies.append(accuracy)
-    if DEBUGGING:
+    if CLIENT_DEBUG:
         print "accuracies for classifier {}:\n{}".format(classifier, accuracies)
     return accuracies
 
@@ -153,77 +154,184 @@ def remove_file_extension(file_name):
         else:
             file_name= file_name + val
 
-    if DEBUGGING:
+    if CLIENT_DEBUG:
         print  "{}{}".format(file_name, extension)
 
     return file_name, extension
 
 
-def shuffle_example(file_directory, file_name):
-    lines = open('{}{}'.format(file_directory, file_name)).readlines()
-    header = lines[0:19]
-    lines = lines[20:]
-
-    print "HEADER"
-    for line in header:
-        print line
-
-    print "LINES:"
-    for line in lines:
-        print line
+def get_data_threshold(file_name):
+    if file_name == DATA_TRAIN_14:
+        return 19
+    elif file_name == DATA_TRAIN_54:
+        return 59
+    else:
+        print "You need to update data threshold function to support the file {}".format(file_name)
+        exit(1)
 
 
-    random.shuffle(lines)
+def shuffle_file_return_header_and_data_rows(file_directory, file_name):
+    all_lines = open('{}{}'.format(file_directory, file_name)).readlines()
 
-    print "SHUFFLED LINES:"
-    for line in lines:
-        print line
+    threshold = get_data_threshold(file_name)
 
-    (file_name, file_extension) = remove_file_extension(file_name)
+    header = all_lines[0:threshold]
+    data_lines = all_lines[threshold:]
 
-    open('{}{}test.{}'.format(file_directory, file_name, file_extension), 'w').writelines(header)
-    open('{}{}test.{}'.format(file_directory, file_name, file_extension), 'a').writelines(lines)
+    if CLIENT_DEBUG:
+        print "HEADER"
+        for line in header:
+            print line
+
+    if CLIENT_DEBUG:
+        print "LINES:"
+        for line in data_lines:
+            print line
+
+    random.shuffle(data_lines)
+
+    if CLIENT_DEBUG:
+        print "SHUFFLED LINES:"
+        for line in data_lines:
+            print line
+
+    return header, data_lines
 
 
-def create_train_set_file():
-    pass
+def create_training_set_file(file_directory, file_name, header, lines, training_size_number):
+    (file_name_without_extension, file_extension) = remove_file_extension(file_name)
+
+    new_file_name = "{}{}.{}".format(file_name_without_extension, training_size_number, file_extension)
+
+    # only write the required number of training examples
+    lines = lines[0: training_size_number]
+    if CLIENT_DEBUG:
+        print "EXAMPLE SIZE: {}".format(len(lines))
+
+    open('{}{}'.format(file_directory, new_file_name), 'w').writelines(header)
+    open('{}{}'.format(file_directory, new_file_name), 'a').writelines(lines)
+
+    return new_file_name
+
+
 
 
 #  credit: https://stackoverflow.com/questions/15389768/standard-deviation-of-a-list
 def calculate_std_mean(experiments):
-    arr = numpy.array([A_rank, B_rank, C_rank])
+    # A_rank = [0.8, 0.4, 1.2, 3.7, 2.6, 5.8]
+    # B_rank = [0.1, 2.8, 3.7, 2.6, 5, 3.4]
+    # C_Rank = [1.2, 3.4, 0.5, 0.1, 2.5, 6.1]
+
+    arr = numpy.array(experiments)
+
+    if CLIENT_DEBUG:
+        print "numpy.array:{}".format(arr)
 
     mean = numpy.mean(arr, axis=0)
+
+
 
     # array([0.7, 2.2, 1.8, 2.13333333, 3.36666667,
     #        5.1])
 
     std = numpy.std(arr, axis=0)
 
+
+
     # array([0.45460606, 1.29614814, 1.37355985, 1.50628314, 1.15566239,
     #        1.2083046])
     return (std, mean)
 
 
+N_REPETITIONS = 10
+N_MAX_TRAINING_SIZE = 500
+
+
+def get_label_for_experiment(classifier, test_file_name):
+    if classifier == CLASSIFIER_J48:
+        classifier_label = LABEL_J48_ACCURACY
+    elif classifier == CLASSIFIER_IBk:
+        classifier_label = LABEL_IBK_ACCURACY
+
+    if test_file_name == DATA_TEST_14:
+        feature_label = "14 Features"
+    elif test_file_name == DATA_TEST_54:
+        feature_label = "54 Features"
+
+    return "{} with {}".format(classifier_label, feature_label)
+
+
 def solution_to_part_2():
+    feature_data = [[DATA_TRAIN_14, DATA_TEST_14], [DATA_TRAIN_54, DATA_TEST_54]]
+    classifiers = [CLASSIFIER_J48, CLASSIFIER_IBk]
 
-    for i in range(1, 11):  # 1, 2, 3, ..., 10
-        shuffle_example(WEKA_DATA_PATH, DATA_TRAIN_14)
-        exit(1)
+    four_data_to_be_plotted = []
+    for classifier in classifiers:
+        for feature in feature_data:
+            training_file_name = feature[0]
+            test_file_name = feature[1]
 
-        for j in range(50, 550, 50):  # 50, 100, ..., 500
-            create_train_set_file()
+            if CLIENT_DEBUG:
+                print "\nRunning experiment for {} {} {}\n".format(classifier, training_file_name, test_file_name)
 
-            run_train_test_return_accuracy(CLASSIFIER_J48, all_train_sets[i], all_test_sets[i])
+            mean, std = run_experiment_for_classifier_and_feature_size(classifier, training_file_name, test_file_name)
+
+            if CLIENT_DEBUG:
+                print  "std{}".format(std)
+                print "mean{}".format(mean)
+            four_data_to_be_plotted.append([mean, std, classifier, test_file_name])
+
+    plot_accuracies_with_stderr("Learning Curves", "Training Set Size", "Test Accuracy",
+                                get_label_for_experiment(four_data_to_be_plotted[0][2], four_data_to_be_plotted[0][3]),
+                                list(range(50, N_MAX_TRAINING_SIZE + 50, 50)),
+                                four_data_to_be_plotted[0][0], four_data_to_be_plotted[0][1],
+
+                                get_label_for_experiment(four_data_to_be_plotted[1][2], four_data_to_be_plotted[1][3]),
+                                list(range(50, N_MAX_TRAINING_SIZE + 50, 50)),
+                                four_data_to_be_plotted[1][0], four_data_to_be_plotted[1][1],
+
+                                get_label_for_experiment(four_data_to_be_plotted[2][2], four_data_to_be_plotted[2][3]),
+                                list(range(50, N_MAX_TRAINING_SIZE + 50, 50)),
+                                four_data_to_be_plotted[2][0], four_data_to_be_plotted[2][1],
+
+                                get_label_for_experiment(four_data_to_be_plotted[3][2], four_data_to_be_plotted[3][3]),
+                                list(range(50, N_MAX_TRAINING_SIZE + 50, 50)),
+                                four_data_to_be_plotted[3][0], four_data_to_be_plotted[3][1]
+                                )
 
 
-    (std, mean) = calculate_std_mean(experiments)
+def run_experiment_for_classifier_and_feature_size(classifier, training_file_name, test_file_name):
+    iteration_results = []
+    for iteration_number in range(1, N_REPETITIONS + 1):  # 1, 2, 3, ..., 10
+        if CLIENT_DEBUG:
+            print "\nITERATION: {}".format(iteration_number)
+            print "shuffling"
 
+        (header, data_rows) = shuffle_file_return_header_and_data_rows(WEKA_DATA_PATH, training_file_name)
 
+        accuracies_vs_training_size = []
+        for training_size_number in range(50, N_MAX_TRAINING_SIZE + 50, 50):  # 50, 100, ..., 500
+            new_training_file_name = create_training_set_file(WEKA_DATA_PATH, training_file_name,
+                                                              header, data_rows, training_size_number)
+
+            accuracy = run_train_test_return_accuracy(classifier, new_training_file_name, test_file_name)
+            accuracies_vs_training_size.append(accuracy)
+
+            if CLIENT_DEBUG:
+                print "accuracy = {}".format(accuracy)
+                print "accuracies = {}".format(accuracies_vs_training_size
+                                               )
+        accuracies_vs_training_size = map(float, accuracies_vs_training_size)
+
+        iteration_results.append(accuracies_vs_training_size)
+    if CLIENT_DEBUG:
+        print "\nall {} iteration results:{}".format(N_REPETITIONS, iteration_results)
+    (std, mean) = calculate_std_mean(iteration_results)
+    return mean, std
 
 
 set_environment()
 test_environment()
 
-
+solution_to_part_1()
 solution_to_part_2()
--- plot.py
+++ plot.py
@@ -14,5 +14,31 @@ def plot_accuracies(l_title, l_axis_x, l_axis_y, l1, x1, y1, l2, x2, y2):
 
     plt.axis([0, 100, 0, 100])
     plt.legend(loc='best')
-    plt.show()
+    plt.show(block=False)
+    plt.savefig('part1_accuracies.png')
 
+
+
+def plot_accuracies_with_stderr(l_title, l_axis_x, l_axis_y, l1, x1, y1, y1err, l2, x2, y2, y2err,
+                                l3, x3, y3, y3err, l4, x4, y4, y4err):
+
+    plt.errorbar(x1,  y1, yerr=y1err, color='r', label=l1, capsize=5)
+    plt.plot(x1, y1, 'ro')
+
+    plt.errorbar(x2,  y2, yerr=y2err, color='b', label=l2, capsize=5)
+    plt.plot(x2, y2, 'bo')
+
+    plt.errorbar(x3,  y3, yerr=y3err, color='g', label=l3, capsize=5)
+    plt.plot(x3, y3, 'gs')
+
+    plt.errorbar(x4,  y4, yerr=y4err, color='y', label=l4, capsize=5)
+    plt.plot(x4, y4, 'ys')
+
+    plt.title(l_title)
+    plt.xlabel(l_axis_x)
+    plt.ylabel(l_axis_y)
+
+    plt.axis([0, 550, 0, 100])
+    plt.legend(loc='best')
+    plt.show(block=False)
+    plt.savefig('./accuracies_with_stderr.png')
