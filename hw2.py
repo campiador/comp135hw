53c9344a735cb55e940558d1e92d9bb27ff329cb
campiador <behnam.heydarshahi@gmail.com>
2017-10-11 21:30:18 -0400

    vocabulary calculates probabilities

--- hw2.py
+++ hw2.py
@@ -6,22 +6,39 @@
 # This code implements Naive Bayes algorithm with smoothing
 # We then evaluate it using cross validation on the task of sentiment analysis
 
+from __future__ import division
 from vocabulary import Vocabulary
 from word import Word
 import re
+from math import log
 
+
+# CONSTANTS
+CLASS_NEGATIVE = 0
+CLASS_POSITIVE = 1
+
+DATASET_FILE_YELP = "yelp_labelled.txt"
+DATASET_FILE_IMDB = "imdb_labelled.txt"
+DATASET_FILE_AMAZON = "amazon_cells_labelled.txt"
+
+# CONTROL VARIABLES
 DEBUG_DEVELOPER = 1
 DEBUG_VERBOSE = 0
 ON_SERVER = 0
 
-DATASET_FILE_YELP = "yelp_labelled.txt"
+SANITIZE_LINES = 1
+SANITIZE_TOKENS = 1
+
+SMOOTHING_FACTOR = 1
 
 
-# Don't put a "/" in the end of the path
-if ON_SERVER:
-    INPUT_FILES_DIR = "."
-else:  # On local machine
-    INPUT_FILES_DIR = "./sentiment labelled sentences"
+def set_environment():
+    global INPUT_FILES_DIR
+    # Don't put a "/" in the end of the path
+    if ON_SERVER:
+        INPUT_FILES_DIR = "."
+    else:  # On local machine
+        INPUT_FILES_DIR = "./sentiment labelled sentences"
 
 
 # Credit: https://stackoverflow.com/questions/10017147/removing-a-list-of-characters-in-string
@@ -35,6 +52,15 @@ def sanitize_token(token):
     return token.lower()
 
 
+def probability_of_word_given_class(word, class_value, vocab, smoothing_factor):
+    n_w_in_c = vocab.get_word_count_given_class(word, class_value)
+    n_c = vocab.get_total_count_for_class(class_value)
+    v = vocab.get_vocabulary_size()
+    m = smoothing_factor
+    print "(n_w_in_c + m)/(n_c + m*v) = ({} + {})/({} + {}*{}) ".format(n_w_in_c, m, n_c, m, v)
+    return (n_w_in_c + m)/(n_c + m*v)
+
+
 def read_file_to_vocab(file_dir, file_name):
     vocab = Vocabulary()
 
@@ -46,7 +72,8 @@ def read_file_to_vocab(file_dir, file_name):
             print "{}: {}".format(index, line)
 
     # remove punctuations from input
-    file_lines = list(map(sanitize_line, file_lines))
+    if SANITIZE_LINES:
+        file_lines = list(map(sanitize_line, file_lines))
 
     for index, line in enumerate(file_lines):
         if DEBUG_DEVELOPER:
@@ -58,7 +85,10 @@ def read_file_to_vocab(file_dir, file_name):
 
         # truncate the class value from line
         line_word_tokens = tokens_and_class_value[0:(len(tokens_and_class_value)-1)]
-        line_word_tokens = list(map(sanitize_token, line_word_tokens))
+
+        # convert all words to lowercase
+        if SANITIZE_TOKENS:
+            line_word_tokens = list(map(sanitize_token, line_word_tokens))
 
         if DEBUG_VERBOSE:
             print "class value for line {}: {}".format(line_word_tokens, line_class_value)
@@ -66,8 +96,21 @@ def read_file_to_vocab(file_dir, file_name):
         for word_occurrence in line_word_tokens:
             vocab.add_word_to_vocabulary(word_occurrence, line_class_value)
 
-    # vocab = Vocabulary()
+    return vocab
 
-read_file_to_vocab(INPUT_FILES_DIR, DATASET_FILE_YELP)
+
+def unit_tests(vocab):
+    vocab.unit_test_no_0_0()
+
+set_environment()
+vocabulary = read_file_to_vocab(INPUT_FILES_DIR, DATASET_FILE_YELP)
+# print vocabulary.get_vocabulary_size()
+# print vocabulary.get_total_count_for_class(CLASS_NEGATIVE)
+# print vocabulary.get_total_count_for_class(CLASS_POSITIVE)
+# print vocabulary.get_word_count_given_class("terrible", CLASS_POSITIVE)
+print probability_of_word_given_class("terrible", CLASS_NEGATIVE, vocabulary, SMOOTHING_FACTOR)
+
+unit_tests(vocabulary)
+
 
 # w = Word("h", 0, 100)
--- vocabulary.py
+++ vocabulary.py
@@ -9,13 +9,17 @@
 #    where the key is token's name,
 #    and the value is an array of count per classes
 #    (I used an array for class counts so that I can use this module later if the classes were more than two.)
+from __future__ import division
 
 import output_class
+
+SKIP_IT = -1
 
 
 class Vocabulary:
     def __init__(self):
         self.words = {}
+        # self.words.setdefault(-1)
 
     def add_word_to_vocabulary(self, word_key, word_class_value):
         if word_key not in self.words:  # first time we see this word
@@ -26,22 +30,42 @@ class Vocabulary:
         class_counts[word_class_value] += 1
         self.words[word_key] = class_counts
 
-    # TODO
-    def get_word_count(self, word_key, class_number):
-
-        return 0
-
-
-    #  TODO
     def get_total_count_for_class(self, class_number):
+        class_count = 0
+        for class_values in iter(self.words.values()):
+            class_count += class_values[class_number]
+        return class_count
 
-        return 0
+    def get_word_count_given_class(self, word_key, class_number):
+        word_counts = self.words.get(word_key)
+        if word_counts is None:
+            #  Word does not exist in vocabulary at all (No_classes). Algorithm should skip it.
+            return SKIP_IT
+        return word_counts[class_number]
 
-    # TODO: Number of unique words
+    def get_class_proportion(self, class_value):
+
+        class_count = self.get_total_count_for_class(class_value)
+        total_count = self.get_total_word_token_count_regardless_of_class()
+
+        return class_count / total_count
+
+    # Number of unique words in vocabulary
     def get_vocabulary_size(self):
+        return len(self.words)
 
-        return 0
+    # Total number of tokens in the original text
+    def get_total_word_token_count_regardless_of_class(self):
+        total_count = 0
+        for class_values in iter(self.words.values()):
+            for class_value in class_values:
+                total_count += class_values
+        return total_count
 
+    def unit_test_no_0_0(self):
+        for count_values in iter(self.words.values()):
+            if count_values[0] == 0 and count_values[1] == 0:
+                print "ERROR: found a word in vocabulary with both class counts equal to 0"
 
 
 
