38fde85729985d5e955ed69fc66e3a9aac5f79b9
campiador <behnam.heydarshahi@gmail.com>
2017-11-05 13:29:57 -0500

    more efficient nmi calculation

--- hw3.py
+++ hw3.py
@@ -224,8 +224,8 @@ def calculate_clustering_nmi(clusters, golden_clusters):
             if n_matrix[i][j] == 0:
                 continue
             else:
-                i_clusters_and_golden_clusters += (n_matrix[i][j] / n_total) * \
-                                                  log((n_matrix[i][j] * n_total) / (a_vector[i] * b_vector[j]))
+                i_clusters_and_golden_clusters += n_matrix[i][j] * log((n_matrix[i][j] * n_total) / (a_vector[i] * b_vector[j]))
+    i_clusters_and_golden_clusters /= n_total
 
     # NMI(U, V)
     nmi = (2 * i_clusters_and_golden_clusters) / (h_clusters + h_golden_clusters)
